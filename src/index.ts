87f8c612471cbb35d2be729d7eedf403d89caa15
Leonardo Kopeski <leonardokop22@gmail.com>
2025-01-11 15:25:17 -0300

    move FileReader to utils and remove old implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { FileReader } from './filereader'
+import { FileReader } from './utils/filereader'
 import { MThd } from './tags'
-
-// MIDI files are organized into data chunks. Each chunk is prefixed with an 8 byte header: 4 byte signature (MThd or MTrk) used to identify the type of chunk followed by a 4 byte size which defines the chunk's length as number of bytes following this header.
 
 const reader = new FileReader('./test/Determination.mid')
 await reader.loadFile()
